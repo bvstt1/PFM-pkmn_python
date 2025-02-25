194efd819c08c210b94a331b53df48d6533c3704
Bastian Velasquez <basti14velasquez@gmail.com>
2025-02-25 14:56:18 -0300

    Fixed types in pokeload, added stats (attack, defense and speed). Inventory function is created and the chose pokemon function is modified

--- pokeload.py
+++ pokeload.py
@@ -1,23 +1,27 @@
 import pickle
+import pprint
+import copy
 from requests_html import HTMLSession
 
 pokemon_base = {
     "name": "",
-    "current_health": 100,
-    "base_health": 1,
-    "level": 1,
-    "type": [],
+    "current_health": None,
+    "base_health": None,
+    "level": None,
     "current_exp": 0,
-    "attacks": []
+    "attack": None,
+    "defense": None,
+    "speed": None
 }
 URL_BASE_POKEMON = "https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/pkmn&pk="
 URL_MOVESET = "https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/movimientos_nivel&pk="
+URL_BASE_HEALTH = "https://www.pokexperto.net/index2.php?seccion=nds/nationaldex/stats&pk="
 
 def get_pokemon(index):
     if index in {30, 50, 83}:
         return {}
 
-    session =HTMLSession()
+    session = HTMLSession()
     url = f"{URL_BASE_POKEMON}{index}"
     new_pokemon = pokemon_base.copy()
     pokemon_page = session.get(url)
@@ -26,15 +30,20 @@ def get_pokemon(index):
     new_pokemon["name"] = pokemon_page.html.find(".mini", first =True).text.split('\n')[0]
 
     # Get Pokémon type
-
+    POKEMON_TYPES = ["normal","fuego","agua","electrico","planta","hielo","lucha","veneno","tierra","volador","psiquico",
+                     "bicho","roca","fantasma","dragon"] # First generation types
     td_type = pokemon_page.html.find("td")[16]
     img_type = td_type.find("img")
-    new_pokemon["type"] = [img.attrs.get("alt") for img in img_type if "alt" in img.attrs]
+    pokemon_type = set([img.attrs.get("alt") for img in img_type if "alt" in img.attrs])
+    new_pokemon["type"] = []
+    for pkmn_type in pokemon_type:
+        if pkmn_type in POKEMON_TYPES:
+            new_pokemon["type"].append(pkmn_type)
 
     # Get Pokémon move set
     url_move_set= f"{URL_MOVESET}{index}"
     pokemon_move_set_page = session.get(url_move_set)
-
+    new_pokemon["attacks"] = []
     for attack_item in (pokemon_move_set_page.html.find(".pkmain")[-1].find(".sortable.left", first=True).find
                        (".check3.bazul")):
         attack ={
@@ -45,8 +54,26 @@ def get_pokemon(index):
         }  
         new_pokemon["attacks"].append(attack)
 
-    return new_pokemon
+    # Get Pokémon base and current health
+    url_base_health = f"{URL_BASE_HEALTH}{index}"
+    pokemon_base_healt_page = session.get(url_base_health)
+    pokemon_base_health =  pokemon_base_healt_page.html.find("td.right", first =True).text.split("\n")[0]
+    new_pokemon["base_health"] = int(pokemon_base_health)
+    new_pokemon["current_health"] = int(pokemon_base_health)
 
+    # Get Pokémon attack
+    pokemon_attack =  pokemon_base_healt_page.html.find("td.right")[2].text.split("\n")[0]
+    new_pokemon["attack"] = int(pokemon_attack)
+
+    # Get Pokémon defense
+    pokemon_defense =  pokemon_base_healt_page.html.find("td.right")[4].text.split("\n")[0]
+    new_pokemon["defense"] = int(pokemon_defense)
+
+    # Get Pokémon speed
+    pokemon_speed =  pokemon_base_healt_page.html.find("td.right")[6].text.split("\n")[0]
+    new_pokemon["speed"] = int(pokemon_speed)
+
+    return new_pokemon
 
 def get_all_pokemon():
     try:
@@ -64,4 +91,3 @@ def get_all_pokemon():
             pickle.dump(all_pokemon, pokefile)
         print("\nTodos los Pokémon han sido descargados")
     return all_pokemon
-
--- pokemon_figth.py
+++ pokemon_figth.py
@@ -1,60 +1,110 @@
 import random
-from pprint import pprint
+import os
+import time
 from pokeload import get_all_pokemon, get_pokemon
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def get_player_profile(pokemon_list):
+def get_player_profile():
     return {
         "player name": input("¿Cual es tu nombre?: "),
-        "pokemon_inventory": [random.choice(pokemon_list) for a in range(3)],
+        "pokemon_inventory": ["Hos"],
         "combats": 0,
         "pokeballs": 0,
         "health_potion": 0,
     }
 
+def inventory(player_profile):
+    while True:
+        try:
+            clear_screen()
+            option = int(input('''+--------------------------------------------------+
+|                    Inventario                    |
++--------------------------------------------------+
+| 1) Datos                                         |
+| 2) Objetos                                       |
+| 3) Equipo pokemon                                |
+| 4) Salir                                         |
++--------------------------------------------------+
+Elija una opción: '''))
+
+            if option == 1:
+                clear_screen()
+                input(f'''+--------------------------------------------------+
+|                    Inventario                    |
++--------------------------------------------------+
+ Nombre: {player_profile['player name']}         
+ Combates: {player_profile['combats']}            
++--------------------------------------------------+
+Presione ENTER para volver al menu de inventario... ''')
+
+            elif option == 2:
+                clear_screen()
+                input(f'''+--------------------------------------------------+
+|                    Inventario                    |
++--------------------------------------------------+
+ Pokebolas: {player_profile['pokeballs']}
+ Pociones: {player_profile['health_potion']}         
++--------------------------------------------------+
+Presione ENTER para volver al menu de inventario... ''')
+
+            elif option == 3:
+                for cont, nombres in enumerate(player_profile['pokemon_inventory'], start=1):
+                    clear_screen()
+                    input(f'''+--------------------------------------------------+
+|                    Inventario                    |
++--------------------------------------------------+
+Pokemon: {player_profile['pokemon_inventory']}        
++--------------------------------------------------+
+Presione ENTER para volver al menu de inventario... ''')
+
+            elif option == 4:
+                clear_screen()
+                print("Saliendo del inventario...")
+                time.sleep(2)
+                break
+
+            else:
+                print("Opción no válida. Por favor, elija entre 1, 2, 3 o 4.")
+
+        except ValueError:
+            print("Entrada inválida. Por favor ingrese un número.")
+            time.sleep(2)
+    return
+
+
 def any_player_pokemon_lives(player_profile):
     return sum([pokemon["current_health"] for pokemon in player_profile["pokemon_inventory"]]) > 0
 
-def choose_pokemon(player_profile):
+
+def choose_pokemon(pokemon_list, player_profile):
     chosen = None
+    random_pokemon = random.sample(pokemon_list, 3)
+    random_names = [pkm["name"] for pkm in random_pokemon]
     while not chosen:
-        print("Elige con que pokemon lucharás")
-        for index in range(len(player_profile["pokemon_inventory"])):
-            print(f"{index} - {get_pokemon_info(player_profile['pokemon_inventory'][index])}")
+        print("Elige uno de estos 3 Pokemon que te ha dejado el Profesor Elias")
+        for cont, nombres in enumerate(random_names, start = 1):
+            print(cont, nombres)
         try:
-            return player_profile['pokemon_inventory'][int(input("¿Cual eliges?: "))]
+            option = int(input("¿Cual eliges?: "))
+            if option == 1:
+                return player_profile['pokemon_inventory'].append(random_names[0])
+            elif option == 2:
+                return player_profile['pokemon_inventory'].append(random_names[1])
+            elif option == 3:
+                return player_profile['pokemon_inventory'].append(random_names[2])
+
         except (ValueError, IndexError):
             print("Opción invalida")
-
-
-
-def get_pokemon_info(pokemon):
-    return f"{pokemon['name']} | lvl {pokemon['level']} | hp {pokemon['current_health']} / {pokemon['base_health']}"
-
-def fight(player_profile, enemy_pokemon):
-    print ("---NUEVO COMBATE---")
-    player_pokemon = choose_pokemon(player_profile)
-    print(f"Contrincantes: {get_pokemon_info(player_pokemon)} VS {get_pokemon_info(enemy_pokemon)}")
-
-    while any_player_pokemon_lives(player_profile) and enemy_pokemon['current_health'] > 0:
-    player_attack(player_pokemon, enemy_pokemon)
-    enemy_attack(enemy_pokemon, player_pokemon)
-
-    print("--FIN DEL COMBATE--")
-    input("Presiona ENTER para continuar...")
+    return
 
 
 def main ():
     pokemon_list = get_all_pokemon()
-    player_profile = get_player_profile(pokemon_list)
-
-    while any_player_pokemon_lives(player_profile):
-        enemy_pokemon = random.choice(pokemon_list)
-        fight(player_profile, enemy_pokemon)
-
-
-    print(f"Has perdido en el combate numero: {player_profile['combats']}")
-
+    player_profile = get_player_profile()
+    choose_pokemon(pokemon_list, player_profile)
+    inventory(player_profile)
 
 if __name__ ==  "__main__":
     main()
